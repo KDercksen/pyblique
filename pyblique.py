819c3994f5c71c6dd0a504f6d0b69a4ab4f5fd87
KDercksen <mail@koendercksen.com>
2014-06-24 21:32:16 +0200

    Implement train/predict, add small data file.
    The ObliqueClassifier can now train and predict. Training is not
    yet actually oblique, though; just axis parallel splits for now.
    
    The xor.data file is just an xor data set, nothing special.

--- pyblique.py
+++ pyblique.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Koen Dercksen - 4215966
 
+# from sklearn.tree import DecisionTreeClassifier as dtc
 import impurity
 import numpy as np
 import sys
@@ -40,34 +41,94 @@ class ObliqueClassifier:
         if data:
             self.train(data)
         self.metric = metric
+        self.tree = {}
 
-    def train(self, data):
-        pass
+    def __fit(self, data):
+        isleaf, leaf = self.__is_leaf_node(data)
+        if len(data) == 0:
+            return -1
+        elif isleaf:
+            return leaf
+        else:
+            # Find best axis parallel split.
+            # First find the best splits for each attribute.
+            splitv = self.__get_split_vector(data)
+            # print("Split vector found: {}".format(splitv))
+            # Now find the index of the best attribute to actually split on
+            index, split = min(enumerate(splitv), key=lambda x: x[1][1])
+            # print("Index, split: {}, {}".format(index, split))
+            # put stuff in the tree
+            tree = {"index": index, "split": split[0]}
+            # split the data for recursive training
+            low, high = self.__split_data(data, index, split[0])
+            # print("Low: {}".format(low))
+            # print("High: {}".format(high))
+            subtree_low = self.__fit(low)
+            tree["low"] = subtree_low
+            subtree_high = self.__fit(high)
+            tree["high"] = subtree_high
+        return tree
 
-    def __possible_splits(self, data, attr):
+    def fit(self, data):
+        self.tree = self.__fit(data)
+
+    def predict(self, record):
+        cls = self.tree
+        while type(cls) is dict:
+            r_val = cls["index"]
+            split = cls["split"]
+            if record[r_val] <= split:
+                cls = cls["low"]
+            else:
+                cls = cls["high"]
+        return cls
+
+    def __best_split_on_attr(self, data, attr):
+        # Will return a tuple of (split test, split value).
         attr_vals = np.sort(data[:, attr])
         weights = np.repeat(1.0, 2) / 2
-        return np.convolve(attr_vals, weights)[1:-1]
-
-    def __best_ap_split(self, data, attr, metric):
+        split_values = np.convolve(attr_vals, weights)[1:-1]
         split_evals = {}
-        for s in self.possible_splits(data, attr):
+        for s in split_values:
             cond = data[:, attr] <= s
             left, right = data[cond], data[~cond]
-            split_evals[s] = metric(left, -1) + metric(right, -1)
+            split_evals[s] = self.metric(left, -1) + self.metric(right, -1)
         # Minimize because we're using gini index
-        return min(split_evals, key=lambda x: split_evals[x])
+        return min(split_evals.items(), key=lambda x: x[1])
 
-    def __get_split_vector(self, data, metric):
+    def __get_split_vector(self, data):
         n_attrs = data.shape[1] - 1
-        result = [self.best_ap_split(data, i, metric) for i in range(n_attrs)]
+        result = [self.__best_split_on_attr(data, i) for i in range(n_attrs)]
         return np.array(result)
 
-    def __split_data(data, attr, threshold):
+    def __split_data(self, data, attr, threshold):
         cond = data[:, attr] <= threshold
         low, high = data[cond], data[~cond]
         return low, high
 
     def __is_leaf_node(self, data):
+        # Returns true/false and the class label (useful if this was a leaf)
         labels = np.array(data[:, -1])
-        return all(label == labels[0] for label in labels)
+        label_all = labels[0]
+        return all(label == label_all for label in labels), label_all
+
+
+# RUNNING SOME TESTS BREH
+print("Loading data...")
+data = get_data("Data/iris.data")
+
+print("Initializing classifier...")
+oc = ObliqueClassifier()
+
+print("Fitting classifier...")
+oc.fit(data)
+
+print("Done training. Calculating error rate...")
+labels = [oc.predict(r) for r in data]
+actual_labels = data[:, -1]
+total = len(labels)
+incorrect = 0
+for i, label in enumerate(labels):
+    if label != actual_labels[i]:
+        incorrect += 1
+print("Error rate: {}".format(incorrect/total))
