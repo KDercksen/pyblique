b868582e5f4c22a24bc0c2a7a3f9302bc3f49346
KDercksen <mail@koendercksen.com>
2014-06-25 01:54:53 +0200

    Start on perturbation algorithm, doesnt quite work yet.
    Just ending the night with a commit... :(

--- pyblique.py
+++ pyblique.py
@@ -3,6 +3,7 @@
 
 import impurity
 import numpy as np
+from random import random
 import sys
 
 
@@ -49,7 +50,7 @@ class ObliqueClassifier:
         cls = self.tree
         while type(cls) is dict:
             splitv = cls["split"]
-            v = sum(a*x for a, x in zip(record[:-1], splitv)) + splitv[-1] > 0
+            v = self.__checkrel(record, splitv) > 0
             if v:
                 cls = cls["high"]
             else:
@@ -70,6 +71,8 @@ class ObliqueClassifier:
             splitv = np.zeros((len(data[0]),))
             splitv[-1] = -split[0]
             splitv[index] = 1
+            # this doesnt quite work yet
+            # splitv = self.__perturb(data, splitv, index)
             tree = {"split": splitv}
             low, high = self.__split_data(data, splitv)
             subtree_low = self.__create_decision_tree(low)
@@ -77,6 +80,45 @@ class ObliqueClassifier:
             subtree_high = self.__create_decision_tree(high)
             tree["high"] = subtree_high
         return tree
+
+    def __checkrel(self, record, splitv):
+        return sum(a*x for a, x in zip(record[:-1], splitv)) + splitv[-1]
+
+    def __calc_u(self, record, splitv, attr):
+        am = splitv[attr]
+        top = am * record[attr] - self.__checkrel(record, splitv)
+        return top / record[attr]
+
+    def __perturb(self, data, splitv, attr):
+        # WARNING SUPER HACKY METHOD
+        # doesnt work yet i think
+        p_repl = 0.3
+        # calculate old impurity
+        low, high = self.__split_data(data, splitv)
+        old_imp = self.metric(low, -1) + self.metric(high, -1)
+        us = np.sort(np.array([self.__calc_u(r, splitv, attr) for r in data]))
+        weights = np.repeat(1.0, 2) / 2
+        split_values = np.convolve(us, weights)[1:-1]
+        splits = {}
+        for s in split_values:
+            cond = data[:, attr] <= s
+            left, right = data[cond], data[~cond]
+            splits[s] = self.metric(left, -1) + self.metric(right, -1)
+        am = min(splits.items(), key=lambda x: x[1])
+        new_splitv = np.array(splitv)
+        new_splitv[attr] = am[0]
+        # calculate new impurity
+        low, high = self.__split_data(data, new_splitv)
+        new_imp = self.metric(low, -1) + self.metric(high, -1)
+        if old_imp > new_imp:
+            return splitv
+        elif old_imp == new_imp:
+            if random() < p_repl:
+                return new_splitv
+            else:
+                return splitv
+        else:
+            return new_splitv
 
     def __best_split_on_attr(self, data, attr):
         # Will return a tuple of (split test, split value).
@@ -99,7 +141,7 @@ class ObliqueClassifier:
     def __split_data(self, data, splitv):
         high, low = [], []
         for i, record in enumerate(data):
-            v = sum(a*x for a, x in zip(record[:-1], splitv)) + splitv[-1] > 0
+            v = self.__checkrel(record, splitv) > 0
             if v:
                 high.append(record)
             else:
@@ -122,17 +164,20 @@ def cross_validate(data, n=5):
     mse_errorsum = 0
     errorsum = 0
     for i in range(len(splits)):
+        print("Iteration #{}".format(i + 1))
         tmpsplits = np.delete(splits, i, axis=0)
-        print("#{}: Creating classifier tree...".format(i + 1))
+        print("Creating classifier tree...")
         oc = ObliqueClassifier()
-        print("#{}: Fitting classifier...".format(i + 1))
+        print("Fitting classifier...")
         oc.fit(np.concatenate(tuple(t for t in tmpsplits)))
         actual_labels = splits[i][:, -1]
+        print("Predicting leftover records...")
         predictions = [oc.predict(r) for r in splits[i]]
         error = error_rate(predictions, actual_labels)
+        print("Local error rate: {:.3f}".format(error))
         errorsum += error
         mse_errorsum += error ** 2
-        print("#{}: Done.".format(i + 1))
+        print("Done.\n")
     return errorsum / n, mse_errorsum / n
 
 
@@ -148,6 +193,6 @@ def error_rate(predictions, labels):
 
 
 # RUNNING SOME TESTS BREH
-data = get_data("Data/xor.data")
-# error, mse = cross_validate(data, n=9)
-# print("Error rate: {:.3f}\nMSE: {:.3f}".format(error, mse))
+data = get_data("Data/iris.data")
+error, mse = cross_validate(data, n=10)
+print("Error rate: {:.3f}\nMSE: {:.3f}".format(error, mse))
